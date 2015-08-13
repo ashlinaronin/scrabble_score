d2b2d14620100ec3b4ef253fca023664ce8142d1
Ashlin Aronin & Sam Martinez <student@epicodus.com>
2015-08-13 12:07:03 -0700

    Add result route to app

--- app/app.php
+++ app/app.php
@@ -8,9 +8,18 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    //Routes here
+
+    //Form route
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('scrabble_score.html.twig');
+        return $app['twig']->render('form.html.twig');
+    });
+
+    //Result route
+    $app->get("/score", function() use ($app) {
+            $my_ScrabbleScore = new ScrabbleScore;
+            $the_score = $my_ScrabbleScore->getScore($_GET['word']);
+
+            return $app['twig']->render('score.html.twig', array('score' => $the_score));
     });
 
     return $app;

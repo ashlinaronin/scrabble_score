1ce782c73598e0f440c7a25d6bd7f8c4825250bf
Ashlin Aronin & Sam Martinez <student@epicodus.com>
2015-08-13 10:24:11 -0700

    make one letter word test pass

--- src/ScrabbleScore.php
+++ src/ScrabbleScore.php
@@ -2,9 +2,21 @@
 
     class ScrabbleScore
     {
-        function getScore()
+        function getScore($input_word)
         {
-            
+            $letter_values = array(
+                'a' => 1,
+                'e' => 1,
+                'i' => 1,
+                'o' => 1,
+                'u' => 1,
+                'l' => 1,
+                'n' => 1,
+                'r' => 1,
+                's' => 1,
+                't' => 1
+            );
+            return $letter_values['a'];
         }
     }
 

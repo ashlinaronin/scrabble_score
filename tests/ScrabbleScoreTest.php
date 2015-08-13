f51513769a5cbdcf300daa4cc85db97401b3f6b3
Ashlin Aronin & Sam Martinez <student@epicodus.com>
2015-08-13 10:16:02 -0700

    add one letter word test

--- tests/ScrabbleScoreTest.php
+++ tests/ScrabbleScoreTest.php
@@ -1,5 +1,19 @@
 <?php
 
     require_once __DIR__."/../src/ScrabbleScore.php";
+    //require_once "src/ScrabbleScore.php";
+
+    class ScrabbleScoreTest extends PHPUnit_Framework_TestCase
+    {
+        function test_getScore_oneLetterWord()
+        {
+            $test_ScrabbleScore = new ScrabbleScore;
+            $input_word = "a";
+
+            $result = $test_ScrabbleScore->getScore($input_word);
+
+            $this->assertEquals(1, $result);
+        }
+    }
 
  ?>
